1b13691a258bd5b7d59d29d80dfecce1b8391d64
Noelia Roberto <noelia.roberto@homail.com>
2023-03-25 17:56:17 -0300

    MODULO 2 - U1

---
+++ M2U1/M2U1 EJERCICIO 1/script.js
@@ -0,0 +1,21 @@
+
+ function btnChanger() {
+   let bg = document.getElementById("container").style.background = "red";
+   let x = document.getElementById("btn-color").style.background = "red";
+   let y = document.getElementById("btn-color").style.color = "gold";
+    
+ }
+
+ function btnChanger1() {
+    let bg = document.getElementById("container").style.background = "blue";
+    let x = document.getElementById("btn-color1").style.background = "blue";
+    let y = document.getElementById("btn-color1").style.color = "gold";
+     
+  }
+
+  function btnChanger2() {
+    let bg = document.getElementById("container").style.background = "violet";
+    let x = document.getElementById("btn-color2").style.background = "violet";
+    let y = document.getElementById("btn-color2").style.color = "gold";
+     
+  }
